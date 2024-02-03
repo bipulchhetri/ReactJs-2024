7e46587593914591e65bdb49ce4d285340c02845
Bipul chhetri <81376867+bipulchhetri@users.noreply.github.com>
2024-02-03 17:31:33 +0000

    First Update

---
+++ Project-2024/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <nav>
+    <ul>
+      <li>Home</li>
+    </ul>
+    </nav>
+    </>
+  )
+}
+
+export default App
